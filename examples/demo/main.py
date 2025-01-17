d16eaefe6860d32cda1809902eed807b7d0dbeff
Daniel Tan <dtch1997@users.noreply.github.com>
2025-01-16 22:01:07 -0800

    update

--- examples/demo/main.py
+++ examples/demo/main.py
@@ -5,7 +5,7 @@ from openai_finetuner.experiment import ExperimentManager
 from openai_finetuner.dataset import DatasetManager
 
 curr_dir = Path(__file__).parent
-project_dir = curr_dir.parent
+project_dir = curr_dir.parents[1]
 load_dotenv(project_dir / ".env")
 
 dataset_manager = DatasetManager()
@@ -23,9 +23,9 @@ dataset_manager.create_dataset(
     id="my_dataset",
     dataset_or_file=dataset
 )
+print(dataset_manager.list_datasets())
 
 runner = ExperimentManager()
-
 runner.create_experiment(
     dataset_id="my_dataset",
     base_model="gpt-4o-mini-2024-07-18",
@@ -33,4 +33,7 @@ runner.create_experiment(
 )
 
 print(runner.list_experiments())
+print(runner.get_experiment_info("my_experiment"))
+
+# NOTE: this will fail if the checkpoint is not ready
 print(runner.get_latest_checkpoint("my_experiment"))
